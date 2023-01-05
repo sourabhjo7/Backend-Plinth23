d6aae90418874da1f62afe31d7c9366031c66dd5
arnit-coder <21ucs029@lnmiit.ac.in>
2023-01-05 20:33:44 +0530

    wip5

--- models/registrationModel.js
+++ models/registrationModel.js
@@ -1,22 +1,5 @@
 // Registration model
 const mongoose = require('mongoose')
-
-const registerSchema = new mongoose.Schema({
-    name : {type : String, required : true, },
-    email : {type : String, required : true, unique : true},
-    phoneNo : {type : String, required : true, unique : true},
-    country : {type : String, required : true},
-    city : {type : String, required : true},
-    residentialAddress : {type : String, required : true},
-    instituteName : {type : String, required : true},
-    instituteAddress : {type : String, required : true},
-    instituteAreaPincode : {type : String, required : true},
-    yearOfStudy : {type : Number, required : true},
-})
-
-module.exports  = mongoose.model("Register", registerSchema)
-
-// const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -29,6 +12,38 @@ const userSchema = new Schema({
   lastName: {type: String, required : true},
   password: {
     type: String,
+  },
+  phoneNo: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  residentialAddress: {
+    type: String,
+    required: true,
+  },
+  instituteName: {
+    type: String,
+    required: true,
+  },
+  instituteAddress: {
+    type: String,
+    required: true,
+  },
+  instituteAreaPincode: {
+    type: String,
+    required: true,
+  },
+  yearOfStudy: {
+    type: String,
+    required: true,
   },
   token: {
     type: String,
