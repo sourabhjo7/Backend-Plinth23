1b006237c38a304e3b09589364b807e1c73f157b
sourabhjo7 <sourabhjoshi9549408165@gmail.com>
2023-01-05 20:27:59 +0530

    wip4

--- models/registrationModel.js
+++ models/registrationModel.js
@@ -15,3 +15,31 @@ const registerSchema = new mongoose.Schema({
 })
 
 module.exports  = mongoose.model("Register", registerSchema)
+
+// const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {type: String, required : true},
+  lastName: {type: String, required : true},
+  password: {
+    type: String,
+  },
+  token: {
+    type: String,
+  },
+  role:{
+    type:String,
+    default:"user"
+  },
+  events:[],
+
+});
+
+module.exports =  mongoose.model("user", userSchema, "user");
+
