fc05547de26ec10fdd1d8003819b77e9cb620846
sourabhjo7 <sourabhjoshi9549408165@gmail.com>
2023-01-06 03:59:33 +0530

    wip 15

--- app.js
+++ app.js
@@ -227,7 +227,7 @@ app.post('/create-team', urlencodedParser,async (req,res) => {
 
 
 
-    const port =process.env.PORT ||3000;
+    const port =3000;
     app.listen(port ,()=>{
         console.log(`server running on port ${port}`);
     });
