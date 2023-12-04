bf8aa0930be400c6d1c0f6b2d8811c7a9714cb1f
sourabhjo7 <sourabhjoshi9549408165@gmail.com>
2023-12-04 13:30:48 +0530

    d

--- controllers/auth.js
+++ controllers/auth.js
@@ -151,7 +151,7 @@ exports.login = async (req, res) => {
       // Setting Up cookies
       const options = {
         expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-        httpOnly: true,
+        // httpOnly: true,
       };
 
       return res.status(200).cookie("token", token, options).json({
